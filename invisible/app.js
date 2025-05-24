aa4dd1fbb10f9c32ea755658994fdebeb2abf85e
nryadav18 <cserajeswaryadav@gmail.com>
2025-05-24 12:42:03 +0530

    google cloud commit access checking let's see

--- invisible/app.js
+++ invisible/app.js
@@ -1365,7 +1365,7 @@ app.get('/', (req, res) => {
 
 
 // Start server
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, '0.0.0.0', () => {
     console.log(`Server is running on port ${PORT}`);
 });
