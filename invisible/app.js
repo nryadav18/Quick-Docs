4f0463ce909036ba849c6ab690876876e5a6e56b
nryadav18 <cserajeswaryadav@gmail.com>
2025-05-24 12:15:54 +0530

    google cloud commit access checking now testing

--- invisible/app.js
+++ invisible/app.js
@@ -973,7 +973,7 @@ app.post('/login', async (req, res) => {
         // Hash the username to find the user
         const trimmedUserName = username.trim();
         const usernameHash = hashValues(trimmedUserName);
-        const user = await User.findOne({ usernameHash : usernameHash });
+        const user = await User.findOne({ usernameHash: usernameHash });
 
         if (!user) {
             return res.status(400).json({ message: 'User not found' });
@@ -1366,4 +1366,7 @@ app.get('/', (req, res) => {
 
 // Start server
 const PORT = process.env.PORT || 8080;
-app.listen(PORT, () => console.log(`✅ Server running on port ${PORT}`));
+app.listen(PORT, '0.0.0.0', () => {
+    console.log(`Server is running on port ${PORT}`);
+});
+
